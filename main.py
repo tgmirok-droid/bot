beb5612c576e24c0af919c13aab2cf25b96cb128
tgmirok-droid <tgmirok@gmail.com>
2026-04-06 22:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from flask import Flask, request
 import os
 
 TOKEN = "8411810943:AAHKjdJ9IVCpJ6tibDd6fwweQZFnq8Nw1kk"
@@ -7,7 +6,6 @@ CHANNEL_LINK = "https://t.me/+RCqrXBwVCi1kNTg0"
 CHAT_LINK = "https://t.me/+YybtW77iiD84ZmYy"
 
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -20,19 +18,5 @@ def start(message):
     
     bot.send_message(message.chat.id, text)
 
-@app.route('/' + TOKEN, methods=['POST'])
-def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '!', 200
-
-if __name__ == '__main__':
-    bot.remove_webhook()
-    WEBHOOK_URL = os.getenv('RAILWAY_PUBLIC_DOMAIN', '')
-    if WEBHOOK_URL:
-        bot.set_webhook(url=f'https://{WEBHOOK_URL}/{TOKEN}')
-        print(f'Webhook set: https://{WEBHOOK_URL}/{TOKEN}')
-    
-    port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+print("Бот запущен и работает!")
+bot.infinity_polling()
